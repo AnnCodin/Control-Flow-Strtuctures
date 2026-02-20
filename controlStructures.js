3ec8bb8af59a526ff095cd09ea3e8d64bebc3e82
AnnCodin <qantoinnette23@gmail.com>
2026-02-20 13:30:54 +0000

    Identify the appropriate season using switch statements

--- controlStructures.js
+++ controlStructures.js
@@ -96,3 +96,114 @@ console.log(validateLogin("john", "pass"));
 console.log(validateLogin("john_doe", "password"));
 console.log(validateLogin("john_doe", "password123"));
 
+//Part 5: Switch Statements
+
+function getDayName(dayNumber) {
+  switch (dayNumber) {
+    case 1:
+      return `${dayNumber}: "Monday"`;
+      break;
+
+    case 2:
+      return `${dayNumber}: "Tuesday"`;
+      break;
+
+    case 3:
+      return `${dayNumber}: "Wednesday"`;
+      break;
+
+    case 4:
+      return `${dayNumber}: "Thursday"`;
+      break;
+
+    case 5:
+      return `${dayNumber}: "Friday"`;
+      break;
+
+    case 6:
+      return `${dayNumber}: "Saturday"`;
+      break;
+
+    case 7:
+      return `${dayNumber}: "Sunday"`;
+      break;
+
+    default:
+      return `${dayNumber}: "Invalid day"`;
+  }
+}
+
+console.log(getDayName(3));
+console.log(getDayName(7));
+console.log(getDayName(10));
+
+//Part 6: Simple Calculator
+
+function calculate(num1, num2, operator) {
+  switch (operator) {
+    case "+":
+      return num1 + num2;
+
+    case "-":
+      return num1 - num2;
+
+    case "*":
+      return num1 * num2;
+
+    case "/":
+      if (num2 === 0) {
+        return "Cannot divide by zero";
+      }
+      return num1 / num2;
+
+    case "%":
+      if (num2 === 0) {
+        return "Cannot divide by zero";
+      }
+      return num1 % num2;
+
+    default:
+      return "Invalid operator";
+  }
+}
+
+console.log(calculate(10, 5, "+"));
+console.log(calculate(10, 5, "-"));
+console.log(calculate(10, 5, "*"));
+console.log(calculate(10, 0, "/"));
+console.log(calculate(10, 5, "^"));
+
+//Part 7: Season Identifier
+
+function getSeason(month) {
+  switch (month.toLowerCase()) {
+    case "december":
+    case "january":
+    case "february":
+      return "Winter";
+      break;
+
+    case "march":
+    case "april":
+    case "may":
+      return "Spring";
+
+    case "june":
+    case "july":
+    case "august":
+      return "Summer";
+
+    case "september":
+    case "october":
+    case "november":
+      return "Fall";
+
+    default:
+      return "Invalid month";
+  }
+}
+
+console.log(getSeason("January"));
+console.log(getSeason("july"));
+console.log(getSeason("October"));
+console.log(getSeason("Smarch"));
