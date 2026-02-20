75915a51d23abae40c2429727eafc8a2181c2846
AnnCodin <qantoinnette23@gmail.com>
2026-02-20 12:17:16 +0000

    Calculate grade scores of students

--- controlStructures.js
+++ controlStructures.js
@@ -18,3 +18,27 @@ console.log(classifyTemperature(-3));
 console.log(classifyTemperature(10));
 console.log(classifyTemperature(19));
 console.log(classifyTemperature(35));
+
+//Part 2: Grade Calculator
+
+function calculateGrade(score) {
+  if (score > 100 || score < 0) {
+    return "Invalid score";
+  } else if (score >= 90) {
+    return "A";
+  } else if (score >= 80) {
+    return "B";
+  } else if (score >= 60) {
+    return "C";
+  } else {
+    return "F";
+  }
+}
+
+console.log(calculateGrade(93));
+console.log(calculateGrade(82));
+console.log(calculateGrade(71));
+console.log(calculateGrade(45));
+console.log(calculateGrade(110));
+
+
